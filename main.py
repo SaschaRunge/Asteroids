acc547a489bfc93dc69f11626d98e8428f59c6b2
SaschaRunge <sascharunge89@yahoo.de>
2025-11-25 19:37:41 +0100

    implemented basic windows

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+
+from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
@@ -6,6 +8,12 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while(True):
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
